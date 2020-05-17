79001b6500501d03577313bf5bdc3d47d5326a24
Anastasia Savich <anastasia.savich.s@gmail.com>
2020-05-17 23:49:05 +0300

    fead: add solutions

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -209,8 +209,12 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  let str = '';
+  str += isStartIncluded ? '[' : '(';
+  str += `${Math.min(a, b)}, ${Math.max(a, b)}`;
+  str += isEndIncluded ? ']' : ')';
+  return str;
 }
 
 
